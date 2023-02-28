d9f9be97d4231f21edda2804e03a92877d9fb3ec
gustavo lima <gustavasd@gmail.com>
2023-02-28 12:07:19 -0300

    FIX list error

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,17 @@
 {
-  'name': 'remake',
-  'category': 'Services',
+  'name': 'Kami_forms',
+  'category': 'Services/Kami_forms',
   'summary': 'Kamico Service Manager',
   'version': '1.0',
   'license': 'GPL-3 or any later version',
   'depends': ['base', 'project', 'website', 'kami_sm'],
   'data': [
-      'templates/remake.xml',
-      'templates/forms_success_page.xml',
+    'templates/forms_success_page.xml',
+    'templates/facades_request.xml',
+    
   ],
   'installable': True,
   'auto_install': False,
   'application': False,
-  'author': 'Gustavo',
+  'author': 'Kami CO IT Team',
 }
